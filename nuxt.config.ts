a1d6c270915bd2b557fbc969c849c2f0483e8aa6
Shige Fukushima <shigeyf@outlook.com>
2023-05-25 03:25:34 +0900

    fix: 🐛 Updated nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,9 @@
+//
+// nuxt.config.ts
+//
+
+import { defineNuxtConfig } from 'nuxt/config'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   rootDir: 'src',
